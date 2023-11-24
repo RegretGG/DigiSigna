7dbdc3353c934a18402a7477251bb8dd8d6bcbc4
RegretGG <gauthamgopinadh@gmail.com>
2023-11-25 00:10:02 +0530

    Added everything

---
+++ Assets/QuestionAndAnswer.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class QuestionAndAnswers
+{
+    public Sprite QuestionImage; // Add this line
+    public string[] Answers;
+    public int CorrectAnswer;
+}
